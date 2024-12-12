24eaa54fb13e58dfd6be8fb9b8360b8a73d77f9d
Ravikumar Padmanaban <ravikumar@aveoninfotech.com>
2024-12-12 17:20:48 +0530

    proxy setup client

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   serverHandlers: [{
-    route: '/api/orders/**',
+    route: '/api/orders',
     handler: '~/server/middleware/proxy/orders.ts'
   }, {
     route: '/api/admin',
     handler: '~/server/middleware/proxy/admin.ts'
-  }]
+  }],
+  ssr: false
 })
