c0ce4eca297d69e293dc1a87c658efdfd57a411f
Ravikumar Padmanaban <ravikumar@aveoninfotech.com>
2024-12-13 15:41:25 +0530

    api proxy shared function

--- server/api/admin/[...].ts
+++ server/api/admin/[...].ts
@@ -1,14 +1,5 @@
-import { joinURL } from 'ufo';
+import apiProxy from '@/utils/api-proxy';
 
 export default defineEventHandler(async (event) => {
-  console.log(event.path);
-  const proxyUrl = useRuntimeConfig().adminProxyUrl;
-  const path = event.path.replace(/^\/api\/admin/, '');
-  const target = joinURL(proxyUrl, path);
-  console.log(target);
-  return proxyRequest(event, target, {
-    headers: {
-      Authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIiwibmFtZSI6IkphbmUgRG9lIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MzQwMjE1ODgsImV4cCI6MTczNDAyNTE4OCwiaXNzIjoiZXhhbXBsZS1hcHAifQ.GYA5Vx15M1mqSPDWQNrOV7Uo5FC6t4IpCn9uPi1uDoU'
-    }
-  });
+  return apiProxy(event, useRuntimeConfig().adminProxyUrl, '^/api/admin');
 });
--- server/api/orders/[...].ts
+++ server/api/orders/[...].ts
@@ -1,12 +1,5 @@
-import { joinURL } from 'ufo';
+import apiProxy from '@/utils/api-proxy';
 
 export default defineEventHandler(async (event) => {
-  const proxyUrl = useRuntimeConfig().orderProxyUrl;
-  const path = event.path.replace(/^\/api\/orders/, '');
-  const target = joinURL(proxyUrl, path);
-  return proxyRequest(event, target, {
-    headers: {
-      Authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIiwibmFtZSI6IkphbmUgRG9lIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MzQwMjE1ODgsImV4cCI6MTczNDAyNTE4OCwiaXNzIjoiZXhhbXBsZS1hcHAifQ.GYA5Vx15M1mqSPDWQNrOV7Uo5FC6t4IpCn9uPi1uDoU'
-    }
-  });
+  return apiProxy(event, useRuntimeConfig().orderProxyUrl, '^/api/orders');
 });
